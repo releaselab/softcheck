19774726846361dbed367304614da105e5e3fbb3
João Santos Reis <joaosreis@outlook.pt>
2019-03-18 20:24:50 +0000

    WIP

--- src/softcheck/il/cfg_node.ml
+++ src/softcheck/il/cfg_node.ml
@@ -1,67 +1,66 @@
 open Batteries
 
-type ident = string
+module Make(E : sig
+    type t
+    val to_string : t -> string
+  end) = struct
+  type expr = E.t
 
-type 'a stmt =
-    Cfg_var_decl of ident node
-  | Cfg_assign of 'a node * 'a node
-  | Cfg_guard of 'a node
-  | Cfg_jump
-  | Cfg_call of 'a node * 'a node list
+  type ident = string
 
-and _ node_data =
-    Stmt : 'a stmt -> ('a stmt) node_data
-  | Decl : ident -> ident node_data
-  | Expr : 'a -> 'a node_data
+  type stmt =
+      Cfg_var_decl of ident node
+    | Cfg_assign of expr node * expr node
+    | Cfg_guard of expr node
+    | Cfg_jump
+    | Cfg_call of expr node * expr node list
 
-and 'a node = {
-  id: int;
-  loc: Common.loc;
-  data: 'a node_data
-}
+  and _ node_data =
+      Stmt : stmt -> stmt node_data
+    | Decl : ident -> ident node_data
+    | Expr : expr -> expr node_data
 
-let counter = ref (-1)
+  and 'a node = {
+    id: int;
+    loc: Common.loc;
+    data: 'a node_data
+  }
 
-let next_counter () =
-  let () = counter := !counter + 1 in
-  !counter
+  type 'a t = 'a node
 
-let create ?loc:(loc=Common.Unknown) data =
-  { id = next_counter (); loc; data }
+  let counter = ref (-1)
 
-let get_node_data : type a. a node -> a = fun n -> match n.data with
-    Stmt s -> s
-  | Decl d -> d
-  | Expr e -> e
+  let next_counter () =
+    let () = counter := !counter + 1 in
+    !counter
 
-let rec stmt_to_string : type a. (a -> string) -> a stmt -> string =
-  fun expr_to_string ->
-  let open Printf in
-  function
-    Cfg_var_decl v -> sprintf "decl %s" (get_node_data v)
-  | Cfg_assign (lv, rv) -> sprintf "%s = %s" (to_string expr_to_string lv)
-                             (to_string expr_to_string rv)
-  | Cfg_guard e -> sprintf "test %s" (to_string expr_to_string e)
-  | Cfg_jump -> sprintf "jump"
-  | Cfg_call (f, args) ->
-      sprintf "%s (%s)" (to_string expr_to_string f)
-        (Utils.sprint_list ~first:"" ~last:"" ~sep:", "
-           (to_string expr_to_string) args)
+  let create ?loc:(loc=Common.Unknown) data =
+    { id = next_counter (); loc; data }
 
-and to_string expr_to_string n =
-  let open Printf in
-  let rec aux n =
-    match n.data with
-      Stmt s -> stmt_to_string expr_to_string s
+  let get_node_data : type a. a node -> a = fun n -> match n.data with
+      Stmt s -> s
     | Decl d -> d
-    | Expr e -> expr_to_string e in
-  aux n
+    | Expr e -> e
 
-module Make_set(E : sig type t end) = struct
-  include Set.Make(struct
-      type t = E.t node
-      let compare x y = compare x.id y.id
-    end)
+  let rec stmt_to_string =
+    let open Printf in
+    function
+      Cfg_var_decl v -> sprintf "decl %s" (get_node_data v)
+    | Cfg_assign (lv, rv) -> sprintf "%s = %s" (to_string lv)
+                               (to_string rv)
+    | Cfg_guard e -> sprintf "test %s" (to_string e)
+    | Cfg_jump -> sprintf "jump"
+    | Cfg_call (f, args) ->
+        sprintf "%s (%s)" (to_string f)
+          (Utils.sprint_list ~first:"" ~last:"" ~sep:", "
+             to_string args)
+
+  and to_string : type a. a node -> string = fun n ->
+    let open Printf in
+    let rec aux (n : a node) =
+      match n.data with
+        Stmt _ -> stmt_to_string (get_node_data n)
+      | Decl d -> d
+      | Expr e -> E.to_string e in
+    aux n
 end
-
-type 'a t = 'a node
